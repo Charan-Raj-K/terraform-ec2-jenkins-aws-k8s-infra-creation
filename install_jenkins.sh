cabd4b6fcef75e4ccbdcca8ca7d2e0af8d034f46
Charan Raj K <40881285+Charan-Raj-K@users.noreply.github.com>
2024-06-21 09:36:50 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # Install Updated packages on linux machine
-sudo yum update
+sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
+sudo yum upgrade
 #sudo yum install jenkins java-1.8.0-openjdk-devel -y
-#sudo amazon-linux-extras install java-openjdk11
-#Java 17 installed and old Java versions commented out
-sudo dnf install java-17-amazon-corretto-devel -y
+sudo amazon-linux-extras install java-openjdk11
 sudo yum install git -y
-sudo yum install nodejs npm -y
 sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven
@@ -21,13 +18,14 @@ sudo systemctl daemon-reload
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
-sudo yum install unzip -y
+sudo yum install unzip
 sudo unzip awscliv2.zip  
 sudo ./aws/install
-#ZAP is installed and can be run as zap.sh
-sudo wget https://github.com/zaproxy/zaproxy/releases/download/v2.14.0/ZAP_2_14_0_unix.sh
-sudo chmod +x ZAP_2_14_0_unix.sh 
-sudo ./ZAP_2_14_0_unix.sh -q
+#ZAP is isntalled at /home/ec2-user/ZAP_2.11.1/zap.sh
+sudo wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2_11_1_unix.sh
+sudo chmod +x ZAP_2_11_1_unix.sh 
+sudo ./ZAP_2_11_1_unix.sh -q
+sudo tar -xvf ZAP_2.11.1_Linux.tar.gz
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
